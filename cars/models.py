ad2d45fc0d16d89babde5652bc6b6e40d3b6483d
brahim024 <ifninos168@gmail.com>
2021-01-13 15:27:32 +0100

    add models defind models fields

--- cars/models.py
+++ cars/models.py
@@ -6,3 +6,6 @@ class Car(models.Model):
 	price = models.PositiveIntegerField()
 	buyer = models.ForeignKey(Buyer,on_delete=models.CASCADE)
 	code = models.CharField(max_length=10,blank=True)
+
+	def __str__(self):
+		return f"{self.name}-{self.price}-{self.buyer}"
