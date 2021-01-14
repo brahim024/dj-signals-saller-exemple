395b1d634842d3d28890c78a8121ef1f04541114
brahim024 <ifninos168@gmail.com>
2021-01-14 13:21:18 +0100

    fix problem in models

--- orders/models.py
+++ orders/models.py
@@ -4,8 +4,8 @@ from cars.models import Car
 class Order(models.Model):
 	name=models.CharField(max_length=200)
 	cars=models.ManyToManyField(Car)
-	total=models.PositiveIntegerField(blank=True)
-	total_price=models.PositiveIntegerField(blank=True)
+	total=models.PositiveIntegerField(blank=True ,null=True)
+	total_price=models.PositiveIntegerField(blank=True ,null=True)
 	active=models.BooleanField(default=True)
 	def __str__(self):
 		return self.name
