1105c39f7af21130966e99307ef80b2d5daa0dfa
brahim024 <ifninos168@gmail.com>
2021-01-14 15:06:04 +0100

    add actions in signals

--- orders/signals.py
+++ orders/signals.py
@@ -7,9 +7,12 @@ from .models import Order
 def m2m_changed_car_orders(sender,instance,action,**kwargs):
 	total=0
 	total_price = 0
-	for car in instance.cars.all():
-		total +=1
-		total_price = car.price
-	instance.total=total
-	instance.total_price = total_price
-	instance.save()
+	if action=='post_add' or 'post_remove':
+		print('its work')
+		print(action)
+		for car in instance.cars.all():
+			total +=1
+			total_price = car.price
+		instance.total=total
+		instance.total_price = total_price
+		instance.save()
